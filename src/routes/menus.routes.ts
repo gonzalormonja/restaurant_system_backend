1ae675e50410f5f1481cc2f88be822f3d8b0f9ff
Gonzalo Monja <gonzalormonja1@gmail.com>
2021-06-16 14:01:07 -0300

    add middleware menus.routes

--- src/routes/menus.routes.ts
+++ src/routes/menus.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { deleteMenu, getMenu, getMenus, postMenu, putMenu } from '../controllers/menus.controllers';
-import { check, param } from 'express-validator';
+import { body, param, query } from 'express-validator';
 import { validate_fields } from '../middlewares/validate_fields';
 import {
   validateCategories,
@@ -14,32 +14,54 @@ const router = Router();
 router.get(
   '/',
   [
-    param('search', 'El campo de search debe ser tipo string').isString(),
-    param('start', 'El campo start debe ser numerico').isInt(),
-    param('limit', 'El campo limit debe ser numerico').isInt(),
-    param('columnOrder', 'El campo columnOrder debe ser tipo string').isString(),
-    param('order', 'Los valores permitidos son ASC o DESC').isIn(['ASC', 'DESC']),
-    param('idCategories').isArray().custom(validateCategories)
+    query('search', 'El campo de search debe ser tipo string').isString(),
+    query('start', 'El campo start debe ser numerico').isInt(),
+    query('limit', 'El campo limit debe ser numerico').isInt(),
+    query('columnOrder', 'El campo columnOrder debe ser tipo string').isString(),
+    query('order', 'Los valores permitidos son ASC o DESC').isIn(['ASC', 'DESC']),
+    query('idCategories').isArray().custom(validateCategories),
+    validate_fields
   ],
   getMenus
 );
-router.get('/:id', getMenu);
+router.get(
+  '/:id',
+  [param('id', 'El id debe ser de tipo numerico y es obligatorio').isInt().notEmpty(), validate_fields],
+  getMenu
+);
 router.post(
   '/',
   [
-    check('state', 'El estado debe ser un booleano valido').isBoolean(),
-    check('name', 'El nombre es obligatorio').isString().notEmpty(),
-    check('short_name', 'El nombre corto es obligatorio').isString().notEmpty(),
-    check('price', 'El precio es obligatorio').isFloat().notEmpty(),
-    check('idCategory').isInt().custom(validateCategory),
-    check('ingrdients').isArray().custom(validateIngredients),
-    check('idCharacteristics').isArray().custom(validateCharacteristics),
-    check('garnishes').isArray().custom(validateGarnishes),
+    body('state', 'El estado debe ser un booleano valido').isBoolean(),
+    body('name', 'El nombre es obligatorio').isString().notEmpty(),
+    body('short_name', 'El nombre corto es obligatorio').isString().notEmpty(),
+    body('price', 'El precio es obligatorio').isFloat().notEmpty(),
+    body('idCategory').isInt().custom(validateCategory),
+    body('ingrdients').isArray().custom(validateIngredients),
+    body('idCharacteristics').isArray().custom(validateCharacteristics),
+    body('garnishes').isArray().custom(validateGarnishes),
     validate_fields
   ],
   postMenu
 );
-router.put('/:id', putMenu);
-router.delete('/:id', deleteMenu);
+router.put(
+  '/:id',
+  [param('id', 'El id debe ser de tipo numerico y es obligatorio').isInt().notEmpty(), validate_fields],
+  putMenu
+);
+router.delete(
+  '/:id',
+  [
+    param('id', 'El id debe ser de tipo numerico y es obligatorio').isInt().notEmpty(),
+    body('state', 'El estado debe ser un booleano valido').isBoolean(),
+    body('price', 'El precio debe ser de tipo numerico').isFloat(),
+    body('idCategory').isInt().custom(validateCategory),
+    body('ingrdients').isArray().custom(validateIngredients),
+    body('idCharacteristics').isArray().custom(validateCharacteristics),
+    body('garnishes').isArray().custom(validateGarnishes),
+    validate_fields
+  ],
+  deleteMenu
+);
 
 export default router;
